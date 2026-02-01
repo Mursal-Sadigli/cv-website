c72725da61a6bd497dd9466a110c2c231f260dc0
Mursel202 <onlysadigli2025@gmail.com>
2026-02-01 14:59:14 +0400

    Fix: Gmail SMTP config for forgot password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -185,7 +185,9 @@ export const forgotPassword = async(req, res) => {
 
         // send email with reset link (async, don't wait)
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
